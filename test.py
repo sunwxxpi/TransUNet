aa762395291044723fc251a5383303e48ba7956f
Sunwoo Pi <ppssww1202@naver.com>
2024-11-18 04:34:28 +0000

    Decrease base learning rate from 0.0001 to 0.00001 in training and testing configurations

--- test.py
+++ test.py
@@ -86,7 +86,7 @@ if __name__ == "__main__":
             'num_classes': 4,
             'max_epochs': 1000,
             'batch_size': 48,
-            'base_lr': 0.0001,
+            'base_lr': 0.00001,
             'z_spacing': 3,
         },
     }
