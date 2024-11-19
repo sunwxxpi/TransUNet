ead1fe6a4a4229b4769649a80a6fd0631b71fad7
Sunwoo Pi <ppssww1202@naver.com>
2024-11-19 05:14:55 +0000

    Refactor argument parsing in test.py; consolidate and update parameters for improved clarity and functionality

--- test.py
+++ test.py
@@ -14,24 +14,6 @@ from utils import test_single_volume
 from networks.transunet.vit_seg_modeling import VisionTransformer as ViT_seg
 from networks.transunet.vit_seg_modeling import CONFIGS as CONFIGS_ViT_seg
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--volume_path', type=str, default='./data/COCA/test_vol_h5', help='root dir for validation volume data')
-parser.add_argument('--dataset', type=str, default='COCA', help='experiment_name')
-parser.add_argument('--num_classes', type=int, default=4, help='output channel of network')
-parser.add_argument('--list_dir', type=str, default='./data/COCA/lists_COCA', help='list dir')
-parser.add_argument('--max_epochs', type=int, default=1000, help='maximum epoch number to train')
-parser.add_argument('--batch_size', type=int, default=96, help='batch_size per gpu')
-parser.add_argument('--img_size', type=int, default=224, help='input patch size of network input')
-parser.add_argument('--is_savenii', action="store_true", help='whether to save results during inference')
-parser.add_argument('--n_skip', type=int, default=3, help='using number of skip-connect, default is num')
-parser.add_argument('--vit_name', type=str, default='ViT-B_16', help='select one vit model')
-parser.add_argument('--test_save_dir', type=str, default='./predictions', help='saving prediction as nii!')
-parser.add_argument('--deterministic', type=int, default=1, help='whether use deterministic training')
-parser.add_argument('--base_lr', type=float, default=0.01, help='segmentation network learning rate')
-parser.add_argument('--seed', type=int, default=1234, help='random seed')
-parser.add_argument('--vit_patches_size', type=int, default=16, help='vit_patches_size, default is 16')
-args = parser.parse_args()
-
 def inference(args, model, test_save_path=None):
     db_test = COCA_dataset(
         base_dir=args.volume_path, 
@@ -65,6 +47,24 @@ def inference(args, model, test_save_path=None):
     
     return "Testing Finished!"
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--volume_path', type=str, default='./data/COCA/test_vol_h5', help='root dir for validation volume data')
+parser.add_argument('--dataset', type=str, default='COCA', help='experiment_name')
+parser.add_argument('--num_classes', type=int, default=4, help='output channel of network')
+parser.add_argument('--list_dir', type=str, default='./data/COCA/lists_COCA', help='list dir')
+parser.add_argument('--max_epochs', type=int, default=1000, help='maximum epoch number to train')
+parser.add_argument('--batch_size', type=int, default=96, help='batch_size per gpu')
+parser.add_argument('--img_size', type=int, default=224, help='input patch size of network input')
+parser.add_argument('--is_savenii', action="store_true", help='whether to save results during inference')
+parser.add_argument('--n_skip', type=int, default=3, help='using number of skip-connect, default is num')
+parser.add_argument('--vit_name', type=str, default='ViT-B_16', help='select one vit model')
+parser.add_argument('--test_save_dir', type=str, default='./predictions', help='saving prediction as nii!')
+parser.add_argument('--deterministic', type=int, default=1, help='whether use deterministic training')
+parser.add_argument('--base_lr', type=float, default=0.01, help='segmentation network learning rate')
+parser.add_argument('--seed', type=int, default=1234, help='random seed')
+parser.add_argument('--vit_patches_size', type=int, default=16, help='vit_patches_size, default is 16')
+args = parser.parse_args()
+
 if __name__ == "__main__":
     if not args.deterministic:
         cudnn.benchmark = True
@@ -87,6 +87,7 @@ if __name__ == "__main__":
             'max_epochs': 300,
             'batch_size': 48,
             'base_lr': 0.00001,
+            'img_size': 512,
             'z_spacing': 3,
         },
     }
