b8c566435799d7913cf41c1d9f8cb1b933baf685
Sunwoo Pi <ppssww1202@naver.com>
2024-12-08 09:29:25 +0000

    Refactor inference function in test.py and add metric computation functions in utils.py for improved performance evaluation

--- test.py
+++ test.py
@@ -13,39 +13,6 @@ from datasets.dataset import COCA_dataset
 from utils import test_single_volume
 from networks.emcad.networks import EMCADNet
 
-def inference(args, model, test_save_path=None):
-    db_test = COCA_dataset(
-        base_dir=args.volume_path, 
-        split="test", 
-        list_dir=args.list_dir,
-    )
-    testloader = DataLoader(db_test, batch_size=1, shuffle=False, num_workers=1)
-    
-    logging.info("{} test iterations per epoch".format(len(testloader)))
-    model.eval()
-    metric_list = 0.0
-    
-    for i_batch, sampled_batch in tqdm(enumerate(testloader, start=1)):
-        h, w = sampled_batch["image"].size()[2:]
-        image, label, case_name = sampled_batch["image"], sampled_batch["label"], sampled_batch['case_name'][0]
-        
-        # 메트릭 계산 및 로깅
-        metric_i = test_single_volume(image, label, model, classes=args.num_classes, patch_size=[args.img_size, args.img_size],
-                                      test_save_path=test_save_path, case=case_name, z_spacing=args.z_spacing)
-        metric_list += np.array(metric_i)
-        logging.info('%s mean_dice %f mean_hd95 %f' % (case_name, np.mean(metric_i, axis=0)[0], np.mean(metric_i, axis=0)[1]))
-    
-    metric_list = metric_list / len(db_test)
-    
-    for i in range(1, args.num_classes):
-        logging.info('Mean class %d mean_dice %f mean_hd95 %f' % (i, metric_list[i-1][0], metric_list[i-1][1]))
-    performance = np.mean(metric_list, axis=0)[0]
-    mean_hd95 = np.mean(metric_list, axis=0)[1]
-    
-    logging.info('Testing performance in best val model: mean_dice : %f mean_hd95 : %f' % (performance, mean_hd95))
-    
-    return "Testing Finished!"
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--volume_path', type=str, default='./data/COCA/test_vol_h5', help='root dir for validation volume data')
 parser.add_argument('--dataset', type=str, default='COCA', help='experiment_name')
@@ -82,6 +49,39 @@ parser.add_argument('--supervision', type=str,
 
 args = parser.parse_args()
 
+def inference(args, model, test_save_path=None):
+    db_test = COCA_dataset(
+        base_dir=args.volume_path, 
+        split="test", 
+        list_dir=args.list_dir,
+    )
+    testloader = DataLoader(db_test, batch_size=1, shuffle=False, num_workers=1)
+    
+    logging.info("{} test iterations per epoch".format(len(testloader)))
+    model.eval()
+    metric_list = 0.0
+    
+    for i_batch, sampled_batch in tqdm(enumerate(testloader, start=1)):
+        h, w = sampled_batch["image"].size()[2:]
+        image, label, case_name = sampled_batch["image"], sampled_batch["label"], sampled_batch['case_name'][0]
+        
+        # 메트릭 계산 및 로깅
+        metric_i = test_single_volume(image, label, model, classes=args.num_classes, patch_size=[args.img_size, args.img_size],
+                                      test_save_path=test_save_path, case=case_name, z_spacing=args.z_spacing)
+        metric_list += np.array(metric_i)
+        logging.info('%s mean_dice %f mean_hd95 %f' % (case_name, np.mean(metric_i, axis=0)[0], np.mean(metric_i, axis=0)[1]))
+    
+    metric_list = metric_list / len(db_test)
+    
+    for i in range(1, args.num_classes):
+        logging.info('Mean class %d mean_dice %f mean_hd95 %f' % (i, metric_list[i-1][0], metric_list[i-1][1]))
+    performance = np.mean(metric_list, axis=0)[0]
+    mean_hd95 = np.mean(metric_list, axis=0)[1]
+    
+    logging.info('Testing performance in best val model: mean_dice : %f mean_hd95 : %f' % (performance, mean_hd95))
+    
+    return "Testing Finished!"
+
 if __name__ == "__main__":
     if not args.deterministic:
         cudnn.benchmark = True
@@ -102,8 +102,8 @@ if __name__ == "__main__":
             'volume_path': './data/COCA/test_vol_h5',
             'list_dir': './data/COCA/lists_COCA',
             'num_classes': 4,
-            'max_epochs': 1,
-            'batch_size': 6,
+            'max_epochs': 300,
+            'batch_size': 48,
             'base_lr': 0.00001,
             'img_size': 224,
             'exp_setting': 'default',
@@ -139,7 +139,6 @@ if __name__ == "__main__":
     best_model_path = glob(os.path.join(snapshot_path, '*_best_model.pth'))[0] # 유일한 best_model 파일 선택
     if not best_model_path:
         raise FileNotFoundError(f"Best model not found at {snapshot_path}")
-    
     net.load_state_dict(torch.load(best_model_path))
     print(f"Loaded best model from: {best_model_path}")
     
