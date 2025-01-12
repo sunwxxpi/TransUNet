eb4047983c58ef8ec5d9a7bead2b5c459307acf3
Sunwoo Pi <ppssww1202@naver.com>
2025-01-12 13:47:17 +0000

    Update ct_normalization parameters for improved image normalization

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -30,7 +30,7 @@ def random_rotate(image, label):
     
     return image, label
 
-def ct_normalization(image, lower=1016, upper=1807, mean=1223.2043595897762, std=133.03651991499345):
+def ct_normalization(image, lower=1017, upper=1801, mean=1222.90087890625, std=132.62820434570312):
     """Normalize the CT image using fixed intensity range and standardization."""
     np.clip(image, lower, upper, out=image)
     image = (image - mean) / max(std, 1e-8)
